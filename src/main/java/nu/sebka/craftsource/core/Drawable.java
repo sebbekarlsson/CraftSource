1d5b0b5a3e50255aeb13dc46298bcee2994cdab6
brandon <brandonbarker767@gmail.com>
2014-08-23 15:27:30 -0400

    Added Drawable

---
+++ src/main/java/nu/sebka/craftsource/core/Drawable.java
@@ -0,0 +1,10 @@
+package nu.sebka.craftsource.core;
+
+/**
+ * Created by brandon on 8/23/14.
+ */
+public interface Drawable {
+    public abstract void tick();
+
+    public abstract void draw();
+}
